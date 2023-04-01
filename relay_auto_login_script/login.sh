55ab9a4a3fdcf9f790a351441e8df367bae90e7c
root <root@baidu.com>
2023-04-01 17:14:20 +0800

    Supports connecting to the server directly after connecting to the jump server using Python 2.

--- relay_auto_login_script/login.sh
+++ relay_auto_login_script/login.sh
@@ -3,15 +3,15 @@ export LC_CTYPE=en_US
 cd "`dirname $0`"
 BASE_HOME=`pwd`
 
-if [ $# -ne 4 ];then
-    echo "Please check the hosts file, the format should be 'host port user password remarks'"
+if [ $# -ne 3 ];then
+    echo "请检查hosts文件，格式为host user password，如果使用门神登录，格式为host DoorGod spider/work/root"
     exit 1
 fi
 
-host=$1
-user=$2
-password=$3
-# relayHost="relay01.baidu.com"
-port=$4
-
-redict $host $user $password $port
+node_host=$1
+node_user=$2
+node_password=$3
+UAuditHost=""
+UAuditUser=""
+UAuditPassword=""
+./auto_login.exp $node_host $node_user $node_password $UAuditHost $UAuditUser $UAuditPassword
