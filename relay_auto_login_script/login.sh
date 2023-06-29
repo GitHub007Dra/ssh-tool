c3e941159ef99b3973b96f32708ffeef7ce7c806
wangjinlong05 <wangjinlong05@baidu.com>
2023-06-29 15:06:46 +0800

    新增门神登录，此版本支持python2

--- relay_auto_login_script/login.sh
+++ relay_auto_login_script/login.sh
@@ -3,14 +3,13 @@ export LC_CTYPE=en_US
 cd "`dirname $0`"
 BASE_HOME=`pwd`
 
-if [ $# -ne 3 ];then
-    echo "请检查hosts文件，格式为host user password，如果使用门神登录，格式为host DoorGod spider/work/root"
+if [ $# -ne 2 ];then
+    echo "请检查hosts文件，格式为host user hostname"
     exit 1
 fi
-# 物理节点 地址 账号 密码 在host文件中配置
+# 物理节点 地址 账号 密码 在host文件中配置 (门神登录不需要密码)
 host=$1
 user=$2
-password=$3
 
 if [ -z "$host" ]; then
     echo "物理节点地址为空，请检查host文件配置"
@@ -20,10 +19,7 @@ if [ -z "$user" ]; then
     echo "物理机用户名为空，请检查host文件配置"
     exit 0
 fi
-if [ -z "$password" ]; then
-    echo "物理机密码为空，请检查host文件配置"
-    exit 0
-fi
+
 
 
 # 跳板机 地址 账号 密码 在login.sh文件配置
